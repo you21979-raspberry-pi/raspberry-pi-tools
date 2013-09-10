f512f31f7adb156afb8ba60d6716d93b8e83988a
you21979 <you2197901@gmail.com>
2013-09-10 12:59:50 +0900

    mod

--- shell/cpu-temp.sh
+++ shell/cpu-temp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/vcgencmd measure_temp | /usr/bin/awk -F"=" '{print $2}' | /usr/bin/awk -F"'" '{print $1}'
+sudo /usr/bin/vcgencmd measure_temp | /usr/bin/awk -F"=" '{print $2}' | /usr/bin/awk -F"'" '{print $1}'
